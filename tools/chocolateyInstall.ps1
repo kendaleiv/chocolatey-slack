eadcefc6b0dce87f2212dfaaa517492218f76c17
Ken Dale <ken@kendaleiv.com>
2016-12-15 19:28:02 -0500

    Add 64-bit version, update package version to 1.1.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,5 +2,6 @@
 $fileType = "exe"
 $silentArgs = "-s"
 $url = "https://slack.com/ssb/download-win"
+$url64 = "https://slack.com/ssb/download-win64"
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
